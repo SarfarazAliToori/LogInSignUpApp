360299c521259118a5aec5004f31725c9b56c9ab
SarfarazAliToori <alifarazzain@gmail.com>
2021-10-22 01:59:04 +0500

    login signup fragments are added.

---
+++ app/src/main/java/com/example/loginsignupapp/LogInAndSignUP.kt
@@ -0,0 +1,11 @@
+package com.example.loginsignupapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogInAndSignUP : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in_and_sign_up)
+    }
+}
